0706bb985109dea127530a4bb5df03ef2320e65b
Prabhu Kumar <prabhukumar636@gmail.com>
2025-10-07 22:25:12 +0530

    patch update

--- src/lib/toast/index.ts
+++ src/lib/toast/index.ts
@@ -1,4 +1,6 @@
 export { default as ToastContainer } from './ToastContainer';
 export { toast } from './toastManager';
 export { ToastType, Position } from './types';
+import "../../index.css";
 export type { ToastOptions, ToastData } from './types';
+export * from "../toast/Toast";
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({ include: ['src/lib'] })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/toast/index.ts'),
+      name: 'ReactToast',
+      fileName: 'index',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
