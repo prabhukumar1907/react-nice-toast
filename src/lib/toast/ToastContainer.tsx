41c05e60040edeb74c1cbc01a75f469393f8c8ea
Prabhu Kumar <prabhu.kumar@shilshatech.com>
2025-10-08 14:24:22 +0530

    Close bug has been fixed

--- src/lib/toast/ToastContainer.tsx
+++ src/lib/toast/ToastContainer.tsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
-import { toastManager } from './toastManager';
-import Toast from './Toast';
-import { Position, type ToastData } from './types';
+import { useState, useEffect } from "react";
+import { toastManager } from "./toastManager";
+import Toast from "./Toast";
+import { Position, type ToastData } from "./types";
 
 interface ToastContainerProps {
   position?: Position;
   maxToasts?: number;
 }
 
-const ToastContainer: React.FC<ToastContainerProps> = ({ 
+const ToastContainer: React.FC<ToastContainerProps> = ({
   position = Position.TOP_RIGHT,
-  maxToasts = 5 
+  maxToasts = 5,
 }) => {
   const [toasts, setToasts] = useState<ToastData[]>([]);
 
@@ -22,32 +22,31 @@ const ToastContainer: React.FC<ToastContainerProps> = ({
   const getPositionStyles = (): string => {
     switch (position) {
       case Position.TOP_LEFT:
-        return 'top-4 left-4';
+        return "top-4 left-4";
       case Position.TOP_CENTER:
-        return 'top-4 left-1/2 -translate-x-1/2';
+        return "top-4 left-1/2 -translate-x-1/2";
       case Position.TOP_RIGHT:
-        return 'top-4 right-4';
+        return "top-4 right-4";
       case Position.BOTTOM_LEFT:
-        return 'bottom-4 left-4';
+        return "bottom-4 left-4";
       case Position.BOTTOM_CENTER:
-        return 'bottom-4 left-1/2 -translate-x-1/2';
+        return "bottom-4 left-1/2 -translate-x-1/2";
       case Position.BOTTOM_RIGHT:
-        return 'bottom-4 right-4';
+        return "bottom-4 right-4";
       default:
-        return 'top-4 right-4';
+        return "top-4 right-4";
     }
   };
 
   const visibleToasts = toasts.slice(-maxToasts);
 
   return (
-    <div 
-      className={`fixed ${getPositionStyles()} z-50 flex flex-col gap-2 
-                  pointer-events-none`} 
+    <div
+      className={`fixed ${getPositionStyles()} z-50 flex flex-col gap-2`}
       aria-live="polite"
       aria-atomic="false"
     >
-      {visibleToasts.map(toastData => (
+      {visibleToasts.map((toastData) => (
         <Toast
           key={toastData.id}
           {...toastData}
